1cd169241d74a873f7c701e6b5d73d6a1309ae28
Lana Ramadan <lanamramadan@gmail.com>
2025-01-16 12:49:20 -0800

    Adjustments to user service & password visibility

--- core/gui/src/app/profile/component/profile.component.ts
+++ core/gui/src/app/profile/component/profile.component.ts
@@ -5,15 +5,27 @@ import { User } from "../../common/type/user";
 @Component({
   selector: "texera-profile",
   templateUrl: "./profile.component.html",
-  styleUrls: ["./profile.component.css"]
+  styleUrls: ["./profile.component.scss"]
 })
 export class ProfileComponent {
   public user: User | undefined;
+  public username : string | undefined;
+  public password : string | undefined;
+  public showPassword: boolean = false;
 
   constructor(
     private userService: UserService,
   ) {
     this.user = this.userService.getCurrentUser();
+
+    if (this.user) {
+      this.username = this.userService.generateSCPUsername(this.user);
+      this.password = this.userService.generateSCPPassword();
+    }
+  }
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
   }
 
 }
